3817cd5178959d6208ab70391fba5015fd1da22c
Gianluca Anselmi <>
2021-04-12 10:11:48 +0000

    update script.js

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
+const sites = [];
+
+const writeSiteList = function(sites){
+
+
+}
+
 const addNewLink = function() {
-console.log("inside addNewLink");
+event.preventDefault();
 const url = $("#form-url").val();
 const title = $("#form-title").val();
 const category = $("#form-category").val();
 console.log(url);
 console.log(title);
 console.log(category);
+
+const newSite = {
+  url: url,
+  title: title,
+  category: category
+
+};
+
+sites.append(newSite);
+
 }
 
 const init = function() {
 
-  $("#form-submit").on("click",addNewLink);
+   $("form").submit(addNewLink);
+ //$("#form-submit").on("click",addNewLink);
 
 }
 
