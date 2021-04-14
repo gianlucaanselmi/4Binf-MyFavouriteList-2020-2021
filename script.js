725c2977539ad2d15848e48406ce5a733d23a5ef
Gianluca Anselmi <>
2021-04-14 21:01:35 +0000

    change variable sites

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
 const sites = [];
 
-const writeSiteList = function(sites){
+const addNewLink = function (event) {
 
+  event.preventDefault();
+  const url = $("#form-url").val();
+  const title = $("#form-title").val();
+  const category = $("#form-category").val();
+
+  console.log(url);
+  console.log(title);
+  console.log(category);
+
+  const newSite = {
+    url: url,
+    title: title,
+    category: category
+  };
+
+  sites.push(newSite);
+  console.log(sites);
+  writeSiteList(sites);
+}
+
+const writeSiteList = function(site){
+
+const aElem = $("<a>").attr("href",site[site.length-1].url).text(site[site.length -1].title);
+const liElem = $("<li>").append(aElem);
+$("#link-list").append(liElem);
 
 }
 
-const addNewLink = function() {
-event.preventDefault();
-const url = $("#form-url").val();
-const title = $("#form-title").val();
-const category = $("#form-category").val();
-console.log(url);
-console.log(title);
-console.log(category);
+const init = function () {
 
-const newSite = {
-  url: url,
-  title: title,
-  category: category
-
-};
-
-sites.append(newSite);
-
-}
-
-const init = function() {
-
-   $("form").submit(addNewLink);
- //$("#form-submit").on("click",addNewLink);
+  $("form").submit(addNewLink);
+  //$("#form-submit").on("click",addNewLink);
 
 }
 
